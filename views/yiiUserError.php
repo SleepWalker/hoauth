d49f1146f2d17b1e055d00e0b5e5d2cec6fcca99
SleepWalker <mybox@udf.su>
2013-05-22 16:17:03 +0200

    added i18n in yiiUserError view

--- views/yiiUserError.php
+++ views/yiiUserError.php
@@ -9,21 +9,21 @@
 switch($errorCode)
 {
 case UserIdentity::ERROR_STATUS_NOTACTIV:
-  $error = 'must be activated! Check your email for details!';
+  $error = HOAuthAction::t('must be activated! Check your email for details!');
   break;
 case UserIdentity::ERROR_STATUS_BAN:
-  $error = 'is banned';
+  $error = HOAuthAction::t('is banned!');
   break;
 }
 ?>
 <div class="form">
   <div class="errorSummary">
-    <p><b>Sorry, but your account <?php echo $error; ?>!</b></p>
+    <p><b><?= HOAuthAction::t('Sorry, but your account') ?> <?php echo $error; ?>!</b></p>
     <p>
     <?php
-    echo CHtml::link('Return to main page', '/') .
+    echo CHtml::link(HOAuthAction::t('Return to main page'), '/') .
       ' | ' .
-      CHtml::link('Return to login page', Yii::app()->getModule('user')->loginUrl);
+      CHtml::link(HOAuthAction::t('Return to login page'), Yii::app()->getModule('user')->loginUrl);
     ?>
     </p>
   </div>
