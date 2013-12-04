a49c3c41b44ddf7c2511a66bf1ac3e9bdadb6d94
mariano.arga <mariano.arga@gmail.com>
2013-12-04 01:47:10 -0300

    Fixes typo

--- widgets/views/link.php
+++ widgets/views/link.php
@@ -8,5 +8,5 @@ $additionalClass = $this->onlyIcons ? 'icon' : '';
 $invitation = Yii::app()->user->isGuest ? HOAuthAction::t('Sign in with') : HOAuthAction::t('Connect with');
 ?>
 <p>
-    <a href="<?php echo Yii::app()->createUrl($this->route . '/oauth', array('provider' => $provider)); ?>" class="zocial <?= $additionalClass . ' ' . strtolower($rovider) ?>"><?= "$invitation $provider"; ?></a>
+    <a href="<?php echo Yii::app()->createUrl($this->route . '/oauth', array('provider' => $provider)); ?>" class="zocial <?= $additionalClass . ' ' . strtolower($provider) ?>"><?= "$invitation $provider"; ?></a>
 </p>
